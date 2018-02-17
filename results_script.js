21af811771bbecbbc0c15488634a13c7b8becafb
DarraghMulhall <darraghmulhall1@gmail.com>
2018-02-17 18:30:48 +0000

    Deleted local canvas.js file, using external url instead

--- results_script.js
+++ results_script.js
@@ -13,7 +13,7 @@ function receiveResults(){
 function renderBarChart(results){
     var chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
-        theme: "light1", // "light1", "light2", "dark1", "dark2"
+        theme: "light2", // "light1", "light2", "dark1", "dark2"
         title:{
             text: "Big Five Personality Traits"
         },
